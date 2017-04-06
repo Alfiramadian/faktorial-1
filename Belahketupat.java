2cf8699990b3bece29902d0bc9d7e5e16f7921b7
Finna Rosalia <finnaros@gmail.com>
2017-04-06 09:33:43 +0700

    Update Belahketupat.java

--- Belahketupat.java
+++ Belahketupat.java
@@ -11,37 +11,29 @@ package belahketupat;
  */
 public class Belahketupat {
 
-   public static void main(String[]args){
-System.out.println("Finna Rosalia (6706154157)");
-System.out.println("");
-int l = 1;
-int tingkat = 10;
-int n = 12/2;
-for(int i=1;i<=n;i++){
-for(int k=n;k>i;k--){
-System.out.print(" ");
-}
-for(int j=1;j<=l;j++){
-System.out.print("*");
-}
-l+=2;
- 
-System.out.println("");
-}
-l -= 4;
- 
-for(int i=(n-1);i>=1;i--){
-for(int k=i;k<=(n-1);k++){
-System.out.print(" ");
-}
-for(int j=l;j>=1;j--){
-System.out.print("*");
-}
-l-=2;
- 
-System.out.println("");
-}
- 
-}
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("input tinggi : ");
+        int n=scn.nextInt();
+//erickstdy.blogspot.com      
+
+    for (int i = 1; i < n; i += 2) {
+      for (int j = 0; j < 4 - i / 2; j++)
+        System.out.print("0");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+        System.out.print("\n");
+    }
+
+    for (int i = n; i > 0; i -= 2) {
+      for (int j = 0; j < 4 - i / 2; j++)
+        System.out.print("0");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+        System.out.print("\n");
+    }
+  }
     
 }
